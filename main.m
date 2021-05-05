4ff888bfdaaee4223e463c0fcd72451030404369
Se Hwan Jeon <sehwan.jeon.9893@gmail.com>
2021-05-05 14:41:10 -0400

    initial commit, structure of files

---
+++ main.m
@@ -0,0 +1,27 @@
+%% metadata
+% Title:            Model Predictive Motion Planning
+% Subject:          16.32 Optimal Control
+% Author:           Se Hwan Jeon
+% Description:      Model predictive planner + controller formulation
+
+clear; clc; close all;
+
+%% dependencies
+%addpath(genpath('casadi/casadi_linux'));   % choose version of casadi depending on OS
+addpath(genpath('casadi/casadi_windows'));
+addpath(genpath('models'));
+addpath(genpath('utilities'));
+import casadi.*
+
+%% flags
+
+
+%% parameters
+
+
+
+%% simulation
+
+
+
+%% animation and plots
